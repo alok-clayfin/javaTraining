e02a25e46de723dd0026b72ed28ce0d5f86af453
alok-clayfin <110982204+alok-clayfin@users.noreply.github.com>
2022-08-23 18:41:52 +0530

    getDayOfWeek()
    23/08/2022

---
+++ Task5.java
@@ -0,0 +1,17 @@
+package clayfinTrainee;
+
+import java.time.LocalDate;
+import java.util.*;
+
+//getDayOfWeek() of local date
+public class Task5 {
+	public static void main(String[] args)
+    {
+        
+        LocalDate dat = LocalDate.parse("2022-08-23");
+  
+        
+        System.out.println(dat.getDayOfWeek());
+    }
+
+}
