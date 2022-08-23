8a80ea16654aade33e9c58f941564959bdb9b740
alok-clayfin <110982204+alok-clayfin@users.noreply.github.com>
2022-08-23 18:57:04 +0530

    getMonthValue(), isLeapYear()
    23/08/2022

---
+++ Task7.java
@@ -0,0 +1,19 @@
+package clayfinTrainee;
+
+import java.time.LocalDate;
+
+public class Task7 {
+	public static void main(String[] args) {
+		
+		//getMonthValue()
+		 LocalDate dat = LocalDate.parse("2022-08-23");
+		 System.out.println(dat.getMonthValue());
+		 
+		 //isLeapYear()
+		 System.out.println(dat.isLeapYear());
+		 
+		
+		 
+	}
+
+}
