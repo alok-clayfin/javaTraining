9412bc878d9a013b3501bc3f0e57ae29a25acf95
alok-clayfin <110982204+alok-clayfin@users.noreply.github.com>
2022-08-24 14:32:40 +0530

    date.compareTo()
    24/08/2022

--- Task10.java
+++ Task10.java
@@ -1,5 +1,6 @@
 package clayfinTrainee;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class Task10 {
@@ -7,11 +8,18 @@ public class Task10 {
 		LocalTime ltime1 = LocalTime.parse("17:52:49");
 		LocalTime ltime2 = LocalTime.parse("13:08:00");
 		
+		LocalDate ldate1 = LocalDate.parse("2022-05-20");
+		LocalDate ldate2 = LocalDate.parse("2022-03-09");
+		
+		
 		int value = ltime1.compareTo(ltime2);
 		//long value = ltime1.compareTo(ltime2);
 		//float value = ltime1.compareTo(ltime2);
 		//double value = ltime1.compareTo(ltime2);
 		System.out.println("Int Value:" + value);
+		System.out.println();
+		int dateValue = ldate1.compareTo(ldate2);
+		System.out.println("Date value: "+ dateValue);
 		  
         if (value > 0)
             System.out.println("LocalTime1 is started");
@@ -20,6 +28,12 @@ public class Task10 {
         else
             System.out.println("LocalTime2 is stopped");
 
+        if (dateValue > 0)
+            System.out.println("LocalDate1 is started");
+        else if (dateValue == 0)
+            System.out.println("LocalDate1 is equal to" + " LocalDate2");
+        else
+            System.out.println("LocalDate2 is stopped");
 	}
 
 }
