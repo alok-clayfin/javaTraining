ed0342960b43f44faac823727f1a9395b4ba3413
alok-clayfin <110982204+alok-clayfin@users.noreply.github.com>
2022-08-24 10:24:47 +0530

    CountObject
    23/08/2022

---
+++ Task2.java
@@ -0,0 +1,30 @@
+package clayfinTrainee;
+
+
+
+public class Task2 {
+	
+	
+		static int count;
+		Task2(){
+			count++;
+		}
+		public static void main(String[] args) {
+			Task2 Obj1 = new Task2();
+			Task2 Obj2 = new Task2();
+			Task2 Obj3 = new Task2();
+			Task2 Obj4 = new Task2();
+			Task2 Obj5 = new Task2();
+			System.out.println("Total Number:"+Task2.count);
+			
+			
+			
+		
+
+	
+	}
+	
+	
+	
+
+}
