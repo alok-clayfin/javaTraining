7776754b1fcba806cb2b96b7cf875090cc9545ed
alok-clayfin <110982204+alok-clayfin@users.noreply.github.com>
2022-08-29 10:36:11 +0530

    Comment
    29/08/2022

---
+++ Comment.java
@@ -0,0 +1,13 @@
+package clayfinTrainee;
+
+public class Comment {
+		public static void main(String[] args) {
+			// The below commented code will be printed
+			// \u000d System.out.println("The commented line was printed here");
+			System.out.println("This is second line");
+			
+			//This is because the unicode indicates new line and the code will be in next line and get printed.
+
+		}
+
+	}
